afbe091fda57f497700bbaa0bfec79bde801e684
Rubén <gutierrezruben657@gmail.com>
2024-08-26 20:28:28 +0200

    xd
    xd

--- bibliotecavirtual/app/Search/page.jsx
+++ bibliotecavirtual/app/Search/page.jsx
@@ -70,8 +70,8 @@ export default function Search() {
                     ))) :
                     <svg version="1.1" x="0px" y="0px" width="200px" className="loadingAnimation"
                       viewBox="0 0 100 100" enable-background="new 0 0 100 100">
-                    <circle fill="none" stroke="#525A66" stroke-width="4" stroke-miterlimit="10" cx="50" cy="50" r="48"/>
-                    <line fill="none" stroke-linecap="round" stroke="#5A7BA5" stroke-width="4" stroke-miterlimit="10" x1="50" y1="50" x2="85" y2="50.5">
+                    <circle fill="none" stroke="#525A66" strokeWidth="4" strokeMiterlimit="10" cx="50" cy="50" r="48"/>
+                    <line fill="none" stroke-linecap="round" stroke="#5A7BA5" strokeWidth="4" strokeMiterlimit="10" x1="50" y1="50" x2="85" y2="50.5">
                       <animateTransform 
                            attributeName="transform" 
                            dur="2s"
@@ -80,7 +80,7 @@ export default function Search() {
                            to="360 50 50"
                            repeatCount="indefinite" />
                     </line>
-                    <line fill="none" stroke-linecap="round" stroke="#5A7BA5" stroke-width="4" stroke-miterlimit="10" x1="50" y1="50" x2="49.5" y2="74">
+                    <line fill="none" strokeLinecap="round" stroke="#5A7BA5" strokeWidth="4" strokeMiterlimit="10" x1="50" y1="50" x2="49.5" y2="74">
                       <animateTransform 
                            attributeName="transform" 
                            dur="15s"
